b664f55b9305dd46efb24492905efaed295b54f5
erik mualana <92783916+ragna999@users.noreply.github.com>
2022-11-25 08:32:26 -0800

    Update pegawai.java

--- pegawai.java
+++ pegawai.java
@@ -2,21 +2,15 @@ public class pegawai {
     String nama;
     double Gajipokok;
 
-    public void setnama(String nama){
-        this.nama = nama;
-    }
-    public String getnama(){
-        return this.nama;
-    }
-    public void setGajipokok(double Gajipokok){
-        this.Gajipokok = Gajipokok;
-    }
-    public double getGajipokok(){
-        return this.Gajipokok;
-    }
-    public void cetakinfo(){
-        System.out.println("nama :" + this.nama);
-        System.out.println("Gaji pokok : Rp." + this.Gajipokok);
 
+    public pegawai(String nama, double gajipokok){
+        this.nama = nama;
+        this.Gajipokok = gajipokok;
+    }
+
+    public void cetakInfo(){
+        System.out.println("=======================================");
+        System.out.println("Nama         : " + this.nama);
+        System.out.println("Gaji POkok   : " + this.Gajipokok);
     }
 }
