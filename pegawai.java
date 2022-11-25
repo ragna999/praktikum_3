a718070d73fcea5d34a8ab010a434781fa605efb
erik mualana <92783916+ragna999@users.noreply.github.com>
2022-11-25 08:33:13 -0800

    Update pegawai.java

--- pegawai.java
+++ pegawai.java
@@ -1,16 +1,26 @@
-public class pegawai {
-    String nama;
-    double Gajipokok;
+public class programer extends pegawai {
+    public programer(String nama, double gajipokok) {
+        super(nama, gajipokok);
+        //TODO Auto-generated constructor stub
+    }
 
+    double bonus;
 
-    public pegawai(String nama, double gajipokok){
-        this.nama = nama;
-        this.Gajipokok = gajipokok;
+    /**
+     * @param nama
+     * @param Gajipokok
+     * @param bonus
+     */
+    public programer(String nama, double Gajipokok, double bonus){
+        super(nama, Gajipokok);
+        this.bonus=bonus;
+
     }
 
     public void cetakInfo(){
-        System.out.println("=======================================");
-        System.out.println("Nama         : " + this.nama);
-        System.out.println("Gaji POkok   : " + this.Gajipokok);
+        System.out.println("Programmer");
+        super.cetakInfo();
+        System.out.println("Bonus        :  "+ this.bonus);
+        System.out.println("======================================");
     }
 }
